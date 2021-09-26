44bf998f93fedf8fc47873751d14a5e2a83fcd32
tonmoyasiquetamal <tonmoyasiquetamal.cse@gmail.com>
2021-09-26 21:35:04 +0600

    Footer are Added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Header from './Components/Header/Header';
 import User from './Components/User/User';
 
+
 /*<----- React BootStrap CDN File ----->*/
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <User></User>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,18 @@
+/*<-----Footer.JS Are Starting Here */
+import { faCopyright } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        // <----- Footer Letter with Copyright Icon ----->
+        <div className="footer">
+            <p className="fw-bold fs-5">CopyRight <FontAwesomeIcon icon = {faCopyright}/> 2021 | All Rights Reserved Tonmoy Asique Tamal</p>
+           
+        </div>
+    );
+};
+
+export default Footer;
+/*<-----Footer.JS Are Ending Here */
